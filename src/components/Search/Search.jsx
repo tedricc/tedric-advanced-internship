b007115989d0d28b491495b24a7daea5e26a940a
tedricc <chow.tedric@gmail.com>
2023-08-30 15:51:23 +0800

    Added search functionality

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 import "./Search.css";
+import axios from "axios";
+import SearchBook from "../ui/SearchBooks/SearchBook";
+import Skeleton from "../ui/Skeleton/Skeleton";
 
 function Search() {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [searchData, setSearchData] = useState([]);
+  const [searching, setSearching] = useState(false);
+  const [completedSearch, setCompletedSearch] = useState(true);
+
+  function handleSearch(event) {
+    if (event.target.value) {
+      setCompletedSearch(false);
+      setSearching(true);
+      setTimeout(() => {
+        setSearchTerm(event.target.value);
+      }, 3000);
+    } else {
+      setSearching(false);
+    }
+  }
+
+  async function getData(input) {
+    if (input) {
+      const { data } = await axios.get(
+        `https://us-central1-summaristt.cloudfunctions.net/getBooksByAuthorOrTitle?search=${input}`
+      );
+      console.log(data);
+      setSearchData(data);
+      setCompletedSearch(true);
+    } else {
+      setSearchData([]);
+      setCompletedSearch(true);
+    }
+  }
+
+  useEffect(() => {
+    getData(searchTerm);
+  }, [searchTerm]);
+
   return (
     <div className="search__background">
       <div className="search__wrapper">
@@ -12,6 +50,7 @@ function Search() {
             <div className="search__input--wrapper">
               <input
                 type="text"
+                onChange={handleSearch}
                 className="search__input"
                 placeholder="Search for books"
                 name=""
@@ -23,6 +62,21 @@ function Search() {
             </div>
           </div>
         </div>
+        {searching ? (
+          <div className="search__books--wrapper">
+            {searchData.length === 0
+              ? new Array(4)
+                  .fill(0)
+                  .map((_, index) => (
+                    <Skeleton id={index} height="80px" width="400px" />
+                  ))
+              : searchData.map((book) => (
+                  <SearchBook key={book.id} book={book} />
+                ))}
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </div>
   );
---
+++ src/components/ui/SearchBooks/SearchBook.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SearchBook.css"
+
+function SearchBook({ book }) {
+  return (
+    <Link to={`/book/${book.id}`} className="search__book--anchor">
+      <figure className="search__img--wrapper">
+        <img src={book.imageLink} alt="" className="search__book--img" />
+      </figure>
+      <div className="search__book--wrapper">
+        <div className="search__book--title">{book.title}</div>
+        <div className="search__book--author">{book.author}</div>
+        {/* <div className="search__book--duration">00:00</div> */}
+      </div>
+    </Link>
+  );
+}
+
+export default SearchBook;
